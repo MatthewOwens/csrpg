8d590dbeea3ad871baffb4bf031ad02a4d7c4085
matthew <matthew@owens.tech>
2018-07-28 17:44:04 +0000

    added include guards

--- common/point.h
+++ common/point.h
@@ -4,7 +4,8 @@
  * floats. This makes the header versitile as it can also be
  * used to store other data. For example: colour information
 */
-
+#ifndef POINT_H
+#define POINT_H
 typedef struct { int x, y; } Point2i;
 typedef struct { int x, y, z ; } Point3i;
 typedef struct { int w, x, y , z; } Point4i;
@@ -12,3 +13,4 @@ typedef struct { int w, x, y , z; } Point4i;
 typedef struct { float x, y; } Point2f;
 typedef struct { float x, y, z; } Point3f;
 typedef struct { float w, x, y, z; } Point4f;
+#endif//POINT_H
--- common/tile.h
+++ common/tile.h
@@ -1,3 +1,5 @@
+#ifndef TILE_H
+#define TILE_H
 #include "point.h"
 
 enum Terrain{
@@ -17,3 +19,4 @@ Tile *tile_init(Point3i pos, enum Terrain ter);
 void tile_cleanup(Tile *t);
 Point3i tile_position(Tile *t);
 enum Terrain tile_terrain(Tile *t);
+#endif//TILE_H
