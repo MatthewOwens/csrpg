efa0168a643dabd81183e6478a2f2ee34576f59c
matthew <matthew@owens.tech>
2018-08-18 22:11:25 +0100

    changed colors so that red is on top of rect, blue is on bottom

--- gl/main.c
+++ gl/main.c
@@ -15,10 +15,10 @@ static Uint32 itime;
 
 static float vertices[] = {
 	// positions		// colors
-	0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.5f,	// top right
-	0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	// bottom right
-	-0.5f, -0.5f, 0.0f,	0.5f, 0.0f, 0.5f,	// bottom left
-	-0.5f, 0.5f, 0.0f,	0.5f, 0.0f, 1.0f	// top left
+	0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	// top right
+	0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	// bottom right
+	-0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	// bottom left
+	-0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f	// top left
 };
 
 static unsigned int indices[] = {
